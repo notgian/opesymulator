21207b84d337273808167d66d8c6259d1d54321a
Gian <85060753+notgian@users.noreply.github.com>
2026-09-20 22:08:28 +0800

    feat: added files and simple main code. Adapted from marquee console code.

---
+++ README.md
@@ -0,0 +1,11 @@
+# CSOPESYMULATOR
+An OS Emulator made for CSOPESY
+
+## Manual Build
+Manual building and running with g++
+
+*NOTE: will update for Windows soon!!*
+
+```bash
+g++ main.cpp -o main.out && ./main.out  
+```
---
+++ main.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <functional>
+
+// to make our lives easier
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
+
+void print_welcome() {
+    cout << endl << endl;
+    cout << " /$$      /$$ /$$$$$$$$ /$$        /$$$$$$   /$$$$$$  /$$      /$$ /$$$$$$$$" << endl; 
+    cout << "| $$  /$ | $$| $$_____/| $$       /$$__  $$ /$$__  $$| $$$    /$$$| $$_____/" << endl;
+    cout << "| $$ /$$$| $$| $$      | $$      | $$  \\__/| $$  \\ $$| $$$$  /$$$$| $$      " << endl;
+    cout << "| $$/$$ $$ $$| $$$$$   | $$      | $$      | $$  | $$| $$ $$/$$ $$| $$$$$   " << endl;
+    cout << "| $$$$_  $$$$| $$__/   | $$      | $$      | $$  | $$| $$  $$$| $$| $$__/   " << endl;
+    cout << "| $$$/ \\  $$$| $$      | $$      | $$    $$| $$  | $$| $$\\  $ | $$| $$      " << endl;
+    cout << "| $$/   \\  $$| $$$$$$$$| $$$$$$$$|  $$$$$$/|  $$$$$$/| $$ \\/  | $$| $$$$$$$$" << endl;
+    cout << "|__/     \\__/|________/|________/ \\______/  \\______/ |__/     |__/|________/" << endl;
+    cout << endl << endl;
+    cout << "CSOPESY Emulator" << endl;
+    cout << "[NAMES] [S03]" << endl;
+    cout << endl;
+}
+
+enum class CommandStatus {
+    Success =  0,
+    ExitRequest = 1,
+    UnknownCommand = -1,
+};
+
+/**
+ * Acts as a dispatcher for each command call
+ *
+ * TODO: this will likely be replaced by something more structured
+ *
+ *
+ * Quick guide on the return codes:
+ *
+ */
+CommandStatus cmd_dispatch(const string& command) {
+    static const std::unordered_map<std::string, std::function<CommandStatus()>> handlers = {
+        {"exit", []() { return CommandStatus::ExitRequest; }},
+        {"help", []() { 
+            // TODO: Implement help display
+            return CommandStatus::Success; 
+        }}
+    };
+
+    auto it = handlers.find(command);
+    if (it != handlers.end()) {
+        return it->second(); // Execute handler
+    }
+
+    return CommandStatus::UnknownCommand;
+}
+
+
+int main (int argc, char *argv[]) {
+    print_welcome();
+
+    bool running = true;
+    string cmdText = "Command";
+
+    while (running) {
+        cout << cmdText << "> ";
+
+        string inputCommand;
+        if (!getline(cin, inputCommand)) break;
+        cout << endl;
+
+        CommandStatus status = cmd_dispatch(inputCommand);
+        switch (status) {
+            case CommandStatus::UnknownCommand:
+                cout << "Command not found: " << inputCommand << endl;
+                break;
+            case CommandStatus::ExitRequest:
+                cout << "EXIT signal received" << endl;
+                cout << "bye bye" << endl;
+                running = false;
+                break;
+            // Success. Do nothing (for now)
+            case CommandStatus::Success:
+                break;
+        }
+
+    }
+    
+    return 0;
+}
